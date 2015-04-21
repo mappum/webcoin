a3863ef83a0d2cf4202e5ab174a26d1b867e15b8
Matt Bell <mappum@gmail.com>
2015-04-21 15:34:49 -0700

    Wrap WebRTC peer connections with WebPeer

--- bin/bridge.js
+++ bin/bridge.js
@@ -3,6 +3,7 @@
 var p2p = require('bitcore-p2p');
 var Networks = require('bitcore').Networks;
 var messages = new p2p.Messages;
+var WebPeer = require('../lib/webPeer.js');
 
 /*var downloadPeer = null;
 
@@ -24,7 +25,11 @@ function startDownload(peer) {
 var PeerGroup = require('../lib/peerGroup.js');
 var pg = new PeerGroup({ acceptWeb: true, verbose: true });
 pg.on('peerconnect', function(peer) {
-  console.log('Connected to peer:', peer.host+':'+peer.port, peer.subversion);
+  peer.on('ready', function() {
+    var uri = peer.host+':'+peer.port;
+    if(peer instanceof WebPeer) uri = '(WebRTC)';
+    console.log('Connected to peer:', uri, peer.subversion);
+  });
 
   peer.on('disconnect', function() {
     console.log('Disconnected from peer:', peer.host);
