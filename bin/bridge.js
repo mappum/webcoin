df38e089e1d2176fb2598e84796a5d6b006651fe
Matt Bell <mappum@gmail.com>
2015-04-21 14:35:02 -0700

    Initial commit

---
+++ bin/bridge.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var p2p = require('bitcore-p2p');
+var Networks = require('bitcore').Networks;
+var messages = new p2p.Messages;
+
+/*var downloadPeer = null;
+
+function startDownload(peer) {
+  downloadPeer = peer;
+
+  peer.on('headers', function(res) {
+    console.dir(res.headers[0])
+  })
+
+  var message = new messages.GetHeaders({
+    starts: [ '00000000dfd5d65c9d8561b4b8f60a63018fe3933ecb131fb37f905f87da951a' ]
+  });
+
+  console.log(message)
+  peer.sendMessage(message);
+}*/
+
+var PeerGroup = require('../lib/peerGroup.js');
+var pg = new PeerGroup({ acceptWeb: true, verbose: true });
+pg.on('peerconnect', function(peer) {
+  console.log('Connected to peer:', peer.host+':'+peer.port, peer.subversion);
+
+  peer.on('disconnect', function() {
+    console.log('Disconnected from peer:', peer.host);
+  });
+})
+pg.connect();
