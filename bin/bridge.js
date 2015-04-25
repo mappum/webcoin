485bae799f138b7e7dbb4078868c5b598035c11b
Matt Bell <mappum@gmail.com>
2015-04-25 12:32:13 -0700

    Formatted with standard style

--- bin/bridge.js
+++ bin/bridge.js
@@ -1,72 +1,71 @@
 #!/usr/bin/env node
 
-var async = require('async');
-var Networks = require('bitcore').Networks;
+var async = require('async')
+var Networks = require('bitcore').Networks
 
-var PeerGroup = require('../lib/peerGroup.js');
-var BlockStore = require('../lib/blockStore.js');
-var Blockchain = require('../lib/blockchain.js');
-var u = require('../lib/utils.js');
+var PeerGroup = require('../lib/peerGroup.js')
+var BlockStore = require('../lib/blockStore.js')
+var Blockchain = require('../lib/blockchain.js')
 
-var dataPath = './data';
-var network = Networks.livenet;
+var dataPath = './data'
+var network = Networks.livenet
 
-var peers = new PeerGroup({ acceptWeb: true, verbose: true, getTip: getTip });
-var store = new BlockStore({ path: dataPath+'/'+network.name+'.chain' });
-var chain = new Blockchain({ peerGroup: peers, store: store, network: network });
+var peers = new PeerGroup({ acceptWeb: true, verbose: true, getTip: getTip })
+var store = new BlockStore({ path: dataPath + '/' + network.name + '.chain' })
+var chain = new Blockchain({ peerGroup: peers, store: store, network: network })
 
-function getTip() { return chain.tip; }
+function getTip () { return chain.tip }
 
-peers.on('peerconnect', function(peer) {
-  console.log('Connected to peer:', peer.remoteAddress, peer.subversion);
+peers.on('peerconnect', function (peer) {
+  console.log('Connected to peer:', peer.remoteAddress, peer.subversion)
 
-  peer.on('disconnect', function() {
-    console.log('Disconnected from peer:', peer.remoteAddress, peer.subversion);
-  });
+  peer.on('disconnect', function () {
+    console.log('Disconnected from peer:', peer.remoteAddress, peer.subversion)
+  })
 
-  peer.on('getheaders', function(message) {
-    sendHeaders(peer, message);
-  });
-});
-peers.connect();
+  peer.on('getheaders', function (message) {
+    sendHeaders(peer, message)
+  })
+})
+peers.connect()
 
-chain.on('sync', function(tip) {
-  var max = chain.downloadPeer.startHeight;
+chain.on('sync', function (tip) {
+  var max = chain.downloadPeer.startHeight
   console.log('Sync progress:', tip.height + ' / ' + max,
     '(' + (Math.round(tip.height / max * 1000) / 10) + '%)',
-    '-', new Date(tip.header.time * 1000));
-});
-chain.on('synced', function(tip) {
-  console.log('Done syncing');
-});
-chain.sync();
+    '-', new Date(tip.header.time * 1000))
+})
+chain.on('synced', function (tip) {
+  console.log('Done syncing')
+})
+chain.sync()
 
-function sendHeaders(peer, message) {
-  var start = null;
-  var headers = [];
+function sendHeaders (peer, message) {
+  var start = null
+  var headers = []
 
-  function next(err, block) {
-    if(err) return console.error(err);
-    headers.push(block.header);
-    if(headers.length === 2000 || !block.next) {
-      peer.sendMessage(peer.messages.Headers(headers));
-      return;
+  function next (err, block) {
+    if (err) return console.error(err)
+    headers.push(block.header)
+    if (headers.length === 2000 || !block.next) {
+      peer.sendMessage(peer.messages.Headers(headers))
+      return
     }
-    store.get(block.next, next);
+    store.get(block.next, next)
   }
 
-  async.each(message.starts, function(hash, cb) {
-    store.get(hash, function(err, block) {
-      if(err && err.name === 'NotFoundError') return cb(null);
-      if(err) return cb(err);
-      if(block) {
-        start = block;
-        return cb(true);
+  async.each(message.starts, function (hash, cb) {
+    store.get(hash, function (err, block) {
+      if (err && err.name === 'NotFoundError') return cb(null)
+      if (err) return cb(err)
+      if (block) {
+        start = block
+        return cb(true)
       }
-    });
-  }, function(err) {
-    if(err !== true) return console.error(err);
-    if(!start || !start.next) return;
-    store.get(start.next, next);
-  });
+    })
+  }, function (err) {
+    if (err !== true) return console.error(err)
+    if (!start || !start.next) return
+    store.get(start.next, next)
+  })
 }
--- test/blockchain.js
+++ test/blockchain.js
@@ -1,74 +1,77 @@
-var test = require('tape');
-var PeerGroup = require('../lib/peerGroup.js');
-var BlockStore = require('../lib/blockStore.js');
-var Blockchain = require('../lib/blockchain.js');
+var test = require('tape')
+var PeerGroup = require('../lib/peerGroup.js')
+var BlockStore = require('../lib/blockStore.js')
+var Blockchain = require('../lib/blockchain.js')
 
-var storePath = 'data/' + process.pid + '.store';
+var storePath = 'data/' + process.pid + '.store'
 
-test('creating blockchain instances', function(t) {
-  t.plan(3);
+test('creating blockchain instances', function (t) {
+  t.plan(3)
 
-  var peers = new PeerGroup;
+  t.test('create blockchain without required options', function (t) {
+    var chain, store
+    t.throws(function () {
+      chain = new Blockchain()
+    })
+    t.throws(function () {
+      var peers = new PeerGroup()
+      chain = new Blockchain({ peerGroup: peers })
+    })
+    t.throws(function () {
+      store = new BlockStore({ path: storePath })
+      chain = new Blockchain({ store: store })
+    })
+    chain || 0 // HACK to prevent JSHint warnings about not using `chain`
+    store.close(t.end)
+  })
 
-  t.test('create blockchain without required options', function(t) {
-    t.throws(function() {
-      new Blockchain;
-    });
-    t.throws(function() {
-      new Blockchain({ peerGroup: peers });
-    });
-    t.throws(function() {
-      new Blockchain({ store: store });
-    });
-    t.end();
-  });
+  t.test('create blockchain with instantiated BlockStore', function (t) {
+    t.doesNotThrow(function () {
+      var peers = new PeerGroup()
+      var store = new BlockStore({ path: storePath })
+      var chain = new Blockchain({ peerGroup: peers, store: store })
+      chain.store.close(t.end)
+    })
+  })
 
-  t.test('create blockchain with instantiated BlockStore', function(t) {
-    var store = new BlockStore({ path: storePath });
-
-    t.doesNotThrow(function() {
-      new Blockchain({ peerGroup: peers, store: store });
-      store.close(t.end);
-    });
-  });
-
-  t.test('create blockchain with path instead of instantiated BlockStore', function(t) {
-    t.doesNotThrow(function() {
+  t.test('create blockchain with path instead of BlockStore', function (t) {
+    t.doesNotThrow(function () {
+      var peers = new PeerGroup()
       var chain = new Blockchain({
         peerGroup: peers,
         path: storePath
-      });
-      chain.store.close(t.end);
-    });
-  });
-});
+      })
+      chain.store.close(t.end)
+    })
+  })
+})
 
-test('blockchain sync', { timeout: 30 * 1000 }, function(t) {
-  var peers = new PeerGroup;
-  peers.connect();
+test('blockchain sync', { timeout: 30 * 1000 }, function (t) {
+  var peers = new PeerGroup()
+  peers.connect()
 
   var store = new BlockStore({
     path: storePath,
-    reset: function(err) {
-      t.error(err);
+    reset: function (err) {
+      t.error(err)
 
-      var chain = new Blockchain({ peerGroup: peers, store: store });
+      var chain = new Blockchain({ peerGroup: peers, store: store })
 
-      chain.on('syncing', function(peer) {
-        t.ok(peer);
-      });
-      chain.on('synced', function(tip) {
-        t.ok(tip);
-        t.equal(tip.height, 3000);
-      });
-      chain.sync({ to: 3000 }, function(block) {
-        t.ok(block);
-        t.equal(block.height, 3000);
-        t.equal(block.header.hash, '000000004a81b9aa469b11649996ecb0a452c16d1181e72f9f980850a1c5ecce');
+      chain.on('syncing', function (peer) {
+        t.ok(peer)
+      })
+      chain.on('synced', function (tip) {
+        t.ok(tip)
+        t.equal(tip.height, 3000)
+      })
+      chain.sync({ to: 3000 }, function (block) {
+        t.ok(block)
+        t.equal(block.height, 3000)
+        t.equal(block.header.hash, '000000004a81b9aa469b11649996ecb0a452c16d1181e72f9f980850a1c5ecce')
 
-        peers.disconnect();
-        store.close(t.end);
-      });
+        peers.disconnect()
+        store.close(t.end)
+      })
     }
-  });
-});
+  })
+})
