fc7f70d5c8c63d82e3f08710a081116a5514b302
Matt Bell <mappum@gmail.com>
2015-07-05 16:31:19 -0700

    test(Node): make sure blockchain syncs correctly

--- test/node.js
+++ test/node.js
@@ -3,6 +3,7 @@ var Networks = require('bitcore').Networks
 var Node = require('../lib/node.js')
 
 test('Node constructor', function (t) {
+  var HEIGHT = 1000
   var node
 
   t.test('setup', function (t) {
@@ -10,9 +11,19 @@ test('Node constructor', function (t) {
     node = new Node({
       network: Networks.testnet,
       path: 'data/' + process.pid,
-      to: 1000
+      to: HEIGHT
     })
     node.start(t.error)
+  })
+
+  t.test('chain', function (t) {
+    node.chain.on('synced', function () {
+      var tip = node.chain.tip
+      t.equal(tip.height, HEIGHT)
+      t.equal(tip.header.hash, '00000000373403049c5fff2cd653590e8cbe6f7ac639db270e7d1a7503d698df')
+      t.notOk(node.chain.syncing)
+      t.end()
+    })
   })
 
   t.test('teardown', function (t) {
