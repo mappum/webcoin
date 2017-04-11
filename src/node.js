c19cd1eda66f915e69e85622134d6ee81a80dfdc
Matt Bell <mappum@gmail.com>
2017-04-11 12:13:46 -0700

    Added streamBlocks function

--- src/node.js
+++ src/node.js
@@ -5,7 +5,7 @@ const old = require('old')
 const sublevel = require('sublevelup')
 const Blockchain = require('blockchain-spv')
 const { PeerGroup } = require('bitcoin-net')
-const { HeaderStream } = require('blockchain-download')
+const { HeaderStream, BlockStream } = require('blockchain-download')
 const Filter = require('bitcoin-filter')
 const Inventory = require('bitcoin-inventory')
 const pump = require('pump')
@@ -19,6 +19,8 @@ class Node extends EventEmitter {
   constructor (params = reqd('params'), db = reqd('db'), opts = {}) {
     super()
     this.db = sublevel(db)
+
+    this.params = params
 
     this.chain = Blockchain(params.blockchain,
       this.db.sublevel('chain'), opts.chainOpts)
@@ -38,19 +40,39 @@ class Node extends EventEmitter {
   start () {
     // connect to peers
     this.peers.connect(() => {
-      // download headers from peers and pipe into chain
-      this.headers = HeaderStream(this.peers)
-      this.headers.once('tip', () => {
-        var emit = () => this.emit('synced', this.chain.getTip())
-        if (!this.headers.lastHash) return emit()
-        this.chain.once('headers', emit)
+      // wait for filter to be ready
+      this.filter.onceReady(() => {
+        // download headers from peers and pipe into chain
+        this.headers = HeaderStream(this.peers)
+        this.headers.once('tip', () => {
+          var emit = () => this.emit('synced', this.chain.getTip())
+          if (!this.headers.lastHash) return emit()
+          this.chain.once('headers', emit)
+        })
+        pump(
+          this.chain.createLocatorStream(),
+          this.headers,
+          this.chain.createWriteStream(),
+          this._error.bind(this)
+        )
       })
-      pump(
-        this.chain.createLocatorStream(),
-        this.headers,
-        this.chain.createWriteStream(),
-        this._error.bind(this)
-      )
+    })
+    return this
+  }
+
+  streamBlocks (from, onBlock) {
+    if (typeof from === 'function') {
+      onBlock = from
+      from = null
+    }
+    this.chain.onceReady(() => {
+      let blocks = BlockStream(this.peers, {
+        inventory: this.inventory,
+        filtered: true
+      })
+      blocks.on('data', onBlock)
+      blocks.on('error', this._error)
+      this.chain.createReadStream({ from }).pipe(blocks)
     })
     return this
   }
